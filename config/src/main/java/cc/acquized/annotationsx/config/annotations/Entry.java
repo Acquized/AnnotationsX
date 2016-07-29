7fe066e8aeed399c199ab52af0a5ff3e9703137b
Acquized <flare.returns@outlook.com>
2016-07-30 02:05:54 +0800

    Hotfix regarding Config API allowing Entry on a Field

--- config/src/main/java/cc/acquized/annotationsx/config/annotations/Entry.java
+++ config/src/main/java/cc/acquized/annotationsx/config/annotations/Entry.java
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
  * @author Acquized
  * @since Annotations 1.0
  */
-@Target({ElementType.TYPE})
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Entry {
 
